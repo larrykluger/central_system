28afbfdc9fa81a7dbddee102f84e4241eb431f9b
larrykluger <larry@kluger.com>
2023-06-09 13:07:57 +0000

    setup

---
+++ examples/websocket_server_echo_test.py
@@ -0,0 +1,21 @@
+import asyncio
+from websockets.server import serve
+import ssl
+
+import logging
+logging.basicConfig(level=logging.NOTSET) # DEBUG)
+
+ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+ssl_context.load_cert_chain("cert.pem", "cert.pem")
+
+
+
+async def echo(websocket):
+    async for message in websocket:
+        await websocket.send(message)
+
+async def main():
+    async with serve(echo, "", 9000, ssl=ssl_context):
+        await asyncio.Future()  # run forever
+
+asyncio.run(main())
