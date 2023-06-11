bc9885e033f6aab320435f8ec3882fea41d38936
larrykluger <larry@kluger.com>
2023-06-11 19:44:56 +0000

    Use asyncio.create_task

--- central_system/central_system_aryeh.py
+++ central_system/central_system_aryeh.py
@@ -150,6 +150,7 @@ async def create_http_server(csms: CentralSystem):
     await runner.setup()
 
     site = web.TCPSite(runner, "localhost", 8080)
+    print ("Web server active on 8080")
     return site
 
 
@@ -159,7 +160,9 @@ async def main():
     websocket_server = await create_websocket_server(csms)
     http_server = await create_http_server(csms)
 
-    await asyncio.wait([websocket_server.wait_closed(), http_server.start()])
+    ws_task = asyncio.create_task (websocket_server.wait_closed())
+    web_task = asyncio.create_task (http_server.start())
+    await asyncio.wait([ws_task, web_task])
 
 
 if __name__ == "__main__":
